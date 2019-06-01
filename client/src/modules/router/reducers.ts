66e3b41478ff526580055dd204a2eb518c1849dc
Sergey_Kirillov <kirillov_sergey@outlook.com>
2019-06-01 09:40:03 +0400

    раскидал зависимости по местам

--- client/src/modules/router/reducers.ts
+++ client/src/modules/router/reducers.ts
@@ -2,10 +2,11 @@ import { NOT_FOUND, Route } from "redux-first-router";
 import { routesMap } from "./actions";
 import { Action } from "../../types/redux";
 
-const components: { [key: string]: string } = Object.keys(routesMap).reduce(
-  (acc, current) => ({ ...acc, [current]: current }),
-  { [NOT_FOUND]: "NOT_FOUND" }
-);
+export const components: { [key: string]: string } = Object.keys(
+  routesMap
+).reduce((acc, current) => ({ ...acc, [current]: current }), {
+  [NOT_FOUND]: "NOT_FOUND"
+});
 
 export default (state: Route = routesMap.HOME, action: Action) =>
   components[action.type] || state;
